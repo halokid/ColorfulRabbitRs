68ed37826f28a24d14d2d1a40d7421fa6bb573f6
Halokid <Halokid>
2024-07-25 18:38:30 +0800

    fix variable

--- src/datet/datet.rs
+++ src/datet/datet.rs
@@ -1,10 +1,10 @@
 use chrono::{NaiveDateTime, ParseResult};
 use crate::CustomErr;
 
-pub fn convert_datet_to_unixt(datet: String) -> Result<i64, CustomErr> {
+pub fn convert_datet_to_unixt(date_time_str: String) -> Result<i64, CustomErr> {
   let format_str = "%Y-%m-%d %H:%M:%S";
 
-  let parsed_date_time = NaiveDateTime::parse_from_str(date_time_str, format_str);
+  let parsed_date_time = NaiveDateTime::parse_from_str(date_time_str.as_str(), format_str);
   match parsed_date_time {
     Ok(parsed_date_time) => {
       let unix_timestamp = parsed_date_time.timestamp();
